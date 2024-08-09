00d72225d3ae07e34f7465ffe10b270f9f4894b1
DmytroLomako <lomakodmytro@gmail.com>
2024-08-09 12:31:30 +0200

    ability to mix music

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import os
 import pygame
+import random
 
 pygame.mixer.init()
 app = ctk.CTk()
@@ -69,8 +70,28 @@ def repeat_music():
     else:
         button_repeat.configure(text = 'Repeat')
     is_repeat = not is_repeat
+def create_music_list():
+    for i in files:
+        index_music = files.index(i)
+        button_music = ctk.CTkButton(frame_music, text = i.split('.')[0], height = 30, width = 150, command = lambda id = index_music: music_play(id))
+        list_music_button.append(button_music)
+        button_music.grid(row = index_music, column = 0, pady = 10, padx = 10)
 button_repeat = ctk.CTkButton(app, text = 'Repeat', command = repeat_music)
 button_repeat.place(y = 250, x = 375, anchor = 'center')
+def mix_music():
+    global index_music
+    current_music = list_music[index_music]
+    random.shuffle(files)
+    list_music.clear()
+    for i in files:
+        list_music.append(os.path.abspath(__file__ + '/../music/' + i))
+    index_music = list_music.index(current_music)
+    list_music_button.clear()
+    for button in frame_music.winfo_children():
+        button.destroy()
+    create_music_list()
+button_mix = ctk.CTkButton(app, text = 'Mix', width = 70, command = mix_music)
+button_mix.place(y = 250, x = 250, anchor = 'center')
 
 button_prev = ctk.CTkButton(button_frame, text = '<<', width = 20, command = prev_music)
 button_prev.grid(row = 0, column = 0, padx = 10)
@@ -87,22 +108,25 @@ def set_volume(volume):
 volume_slider = ctk.CTkSlider(app, width = 15, height = 200, command = set_volume, orientation = 'vertical')
 volume_slider.place(x = 540, y = 100)
 
-class Music_button:
-    def __init__(self, name, music_id):
-        self.button_music = ctk.CTkButton(frame_music, text = name.split('.')[0], height = 30, width = 150, command = self.music_play)
-        self.music_id = music_id
-    def music_play(self):
-        global index_music, is_pause
-        index_music = self.music_id
-        is_pause = False
-        button_pause.configure(text = 'Pause')
-        load_music()
-        pygame.mixer.music.play()
-for i in files:
-    index_music = files.index(i)
-    button_music = Music_button(i, index_music)
-    list_music_button.append(button_music)
-    button_music.button_music.grid(row = index_music, column = 0, pady = 10, padx = 10)
+def music_play(music_id):
+    global index_music, is_pause
+    index_music = music_id
+    is_pause = False
+    button_pause.configure(text = 'Pause')
+    load_music()
+    pygame.mixer.music.play()
+# class Music_button:
+#     def __init__(self, name, music_id):
+#         self.button_music = ctk.CTkButton(frame_music, text = name.split('.')[0], height = 30, width = 150, command = self.music_play)
+#         self.music_id = music_id
+#     def music_play(self):
+#         global index_music, is_pause
+#         index_music = self.music_id
+#         is_pause = False
+#         button_pause.configure(text = 'Pause')
+#         load_music()
+#         pygame.mixer.music.play()
+create_music_list()
 load_music()
 pygame.mixer.music.play()
 app.after(1000, music_check)
